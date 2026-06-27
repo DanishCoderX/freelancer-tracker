98b5331a4feab6752d319c7f60c1ef34b636911c
DanishCoderX <daanishsaeed593@gmail.com>
2026-06-27 15:49:39 +0500

    Adjust main layout for responsive design

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -4,7 +4,8 @@ export default function Layout({ children }) {
   return (
     <div className="flex min-h-screen">
       <Navbar />
-      <main className="ml-56 flex-1 p-8 min-h-screen">
+      {/* Desktop: offset for sidebar. Mobile: offset for top bar */}
+      <main className="flex-1 lg:ml-56 pt-16 lg:pt-0 p-4 lg:p-8 min-h-screen">
         {children}
       </main>
     </div>
